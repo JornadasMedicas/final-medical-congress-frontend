6fd9809423412193d0bf064534caca8ea1a3f1f2
PressureDraper <warmarines00@hotmail.com>
2026-09-22 16:37:03 -0600

    fix(sedes): actualizando sedes

--- src/helpers/sedes/data.ts
+++ src/helpers/sedes/data.ts
@@ -3,27 +3,11 @@ import { PropsSedes } from "../../interfaces/sedes/ISedes";
 export const sedes2026: PropsSedes[] = [
     {
         id: 1,
-        title: 'CONGRESO DE MEDICINA Y ENFERMERIA',
-        place: 'AUDITORIO "LA CALERA"',
-        address: 'Av. Libertad #84, Centro, Banderilla, Ver.',
-        map: 'https://www.google.com/maps/place/Auditorio+La+Calera/@19.5880245,-96.9367653,21z/data=!4m14!1m7!3m6!1s0x85db2f70dc981183:0x87c40b494ab4247!2sAuditorio+La+Calera!8m2!3d19.5880971!4d-96.9367311!16s%2Fg%2F11hf38_vl3!3m5!1s0x85db2f70dc981183:0x87c40b494ab4247!8m2!3d19.5880971!4d-96.9367311!16s%2Fg%2F11hf38_vl3?entry=ttu&g_ep=EgoyMDI0MTExMS4wIKXMDSoASAFQAw%3D%3D',
-        location: [-96.93671537793244, 19.588090811612958]
-    },
-    {
-        id: 2,
-        title: 'TALLERES',
-        place: 'HOTEL KLIMIT',
-        address: 'Av. Miguel Alemán #310 Col. Laderas de Macuiltepetl, Xalapa, Ver.',
-        map: 'https://www.google.com/maps/place/Hotel+Klimt/@19.5429163,-96.919342,19.72z/data=!4m9!3m8!1s0x85db31e62f9e703d:0x49c95114b908a981!5m2!4m1!1i2!8m2!3d19.5428097!4d-96.9192168!16s%2Fg%2F11ggs3blfr?entry=ttu&g_ep=EgoyMDI0MTExMS4wIKXMDSoASAFQAw%3D%3D',
-        location: [-96.91924225652667, 19.542849287196848]
-    },
-    {
-        id: 3,
-        title: 'CONGRESO DE QUÍMICOS Y ESTOMATOLOGÍA',
-        place: 'Centro de Alta Epecialidad Dr. Rafael Lucio',
-        address: 'Av. Adolfo Ruiz Cortines 2903, Unidad Magisterial, Xalapa, Ver.',
-        map: 'https://www.google.com.mx/maps/place/Centro+de+Alta+Especialidad+%22Dr.+Rafael+Lucio%22/@19.5517662,-96.9384562,17z/data=!3m1!4b1!4m6!3m5!1s0x85db2e16cb33d43b:0xef08a9e7531a4f7d!8m2!3d19.5517612!4d-96.9358813!16s%2Fg%2F1yl452rl7?entry=ttu&g_ep=EgoyMDI0MTExOC4wIKXMDSoASAFQAw%3D%3D',
-        location: [-96.93588130332994, 19.551993716336828]
+        title: 'CONGRESO GENERAL',
+        place: 'HOTEL XALAPA (SALÓN GOBERNADORES Y MARGARITAS)',
+        address: 'Guadalupe Victoria 163, Zona Centro, Centro, 91000 Xalapa-Enríquez, Ver.',
+        map: 'https://www.google.com/maps/place/Gran+Hotel+Xalapa/@19.5308982,-96.9301983,206m/data=!3m1!1e3!4m9!3m8!1s0x85db2e078654c40d:0xd479ebcae355883f!5m2!4m1!1i2!8m2!3d19.5314895!4d-96.9294777!16s%2Fg%2F1tcz_wlm?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D',
+        location: [-96.92947807721428, 19.531518343354062],
     }
 ]
 
